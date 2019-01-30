931aafb0b8145ef8e01fc9991ab3d45866935a1f
Mateusz Sobczuk <43694687+mateoc4@users.noreply.github.com>
2019-01-30 13:44:05 +0100

    Update CardList.js

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -23,26 +23,3 @@ const CardList = ({ robots }) => {
 }
 
 export default CardList;
-
-/*
-
-const CardList = ({ robots }) => {
-    const cardArray = robots.map((user, i) => {
-        return  (
-        <Card
-            key={i}
-            id={robots[i].id}
-            name={robots[i].name}
-            email={robots[i].email}
-            username={robots[i].username}
-        />)
-    })
-    return (
-        <div>
-           {cardArray}
-        </div>
-    )
-}
-export default CardList;
-
-*/
